da4f3b234a50cd990ec8fdb11882e2e975f25326
Brian Johnson <johnson.brian@bcg.com>
2017-12-03 10:41:11 -0600

    Update to Random work AJAX page.  Add error catching and validate input.

--- js/random.js
+++ js/random.js
@@ -6,44 +6,90 @@ $(document).ready(function($){
 		
 		let numWords = $("#num").val();
 		
+		let numWordsInt = 0;
+		
+		let hasParseError = false;
+		let parseErrorMessage = null;
+		
+		// Clear output
+		$("#output").html('');
+		$("#outputError").html('');
+		
+		try {
+			numWordsInt = Number.parseInt(numWords);
+		} catch(ex) {
+			hasParseError = true;
+			parseErrorMessage = 'Exception while parsing number entered';
+		}
+		
+		if (isNaN(numWordsInt)) {
+			if (parseErrorMessage == null) {
+				parseErrorMessage = 'It looks like you didn\'t enter a valid int. Please enter an int and try again.';
+			}
+		
+			$("#outputError").text('Error: ' + parseErrorMessage);	
 
-		
-		$("#output").text(numWords);
-		
-		// http://blog.revathskumar.com/2016/06/why-i-prefer-ajax-promise.html
-		$.ajax({
-			url: 'http://ellypost.com/courses/453/ajax-lab/actions.php',
-			//dataType: 'jsonp',
-			method: "POST",
 			
-			//jsonpCallback: 'callbackFnc',
-			//async: false,
-			//crossDomain: true,
-			
-			data: { 
-				'word-count': 3,
-				'action': 'generate-string'
+		} else {
+
+			$("#output").text('Loading words...');
+
+			// http://blog.revathskumar.com/2016/06/why-i-prefer-ajax-promise.html
+			$.ajax({
+				url: 'http://ellypost.com/courses/453/ajax-lab/actions.php',
+				method: "POST",
+
+				data: { 
+					'word-count': numWordsInt,
+					'action': 'generate-string'
+					}
+				})
+				.done(function(data) {
+
+					let randomWordsReturn = null;
+
+					try {
+						randomWordsReturn = JSON.parse(data)
+					} catch(ex) {
+						alert('error parsing JSON');
+						console.log('parse exception', ex)
+					}
+
+					console.log('ajax success', data);
+
+
+					if (randomWordsReturn.value != null && randomWordsReturn.status == 1) {
+
+						// Change line ending from UNIX to Windows or else the pre whites-space
+						// doesn't seem to work correctly
+						randomWordsReturn = randomWordsReturn.value.replace(/\r/g, '\r\n');
+						
+						$("#output").text(randomWordsReturn);
+
+					} else {
+						
+						$("#outputError").text('Error getting random words (status != 1): ' + randomWordsReturn.value);
+
+					}
+
+					
+				})
+				.fail(function(xhr) {
+					console.log('ajax error', xhr);
 				}
-			})
-			.done(function(data) {
-				
-				let randomWordsReturn = null;
-				
-				try {
-					randomWordsReturn = JSON.parse(data)
-				} catch(ex) {
-					alert('error');
-					console.log('parse exception', ex)
-				}
-				
-				console.log('ajax success', data);
-				
-				$("#output").text(randomWordsReturn.value);
-			})
-			.fail(function(xhr) {
-				console.log('ajax error', xhr);
-			});
+			);
+		}
 	});
+	
+	
+	$("form").submit(function(e){
+	
+		$("#generate").click();
+	
+		e.preventDefault();
+		return false;
+	});
+
 
 
 });
